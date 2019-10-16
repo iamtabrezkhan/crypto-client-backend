e301c3073d47610a963ecc9bdd5d951b82cdbe5e
Tabrez Khan <khan.t.29@gmail.com>
2019-10-17 02:28:33 +0530

    make it better

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 let port = process.env.PORT || 3000;
+const superagent = require('superagent');
+const config = require('./config');
 
 const app = express();
 
@@ -14,6 +16,27 @@ app.use(cors());
 // root
 app.get('/', (req, res, next) => {
     res.send('Hello World');
+})
+
+// get latest listings
+app.get('/listings/latest', (req, res, next) => {
+    let url = config.urls.latestListings;
+    superagent
+    .get(url)
+    .set('X-CMC_PRO_API_KEY', config.apiKey)
+    .end((err, resp) => {
+        // Calling the end function will send the request
+        if(err) {
+            return res.json({
+                status: err.status,
+                success: false
+            })
+        };
+        return res.json({
+            success: true,
+            data: resp.body
+        })
+    });
 })
 
 app.listen(port, () => {
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    urls: {
+        latestListings: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+    },
+    apiKey: '6d64b439-2143-45a3-927f-213dd99ef6b2'
+}
